757aad48661adc850b2e95e9a5a87b197ec65f55
Rick Copeland <rick@arborian.com>
2012-09-11 12:21:02 -0400

    Update prealloc
    Signed-off-by: Rick Copeland <rick@arborian.com>

--- writer.py
+++ writer.py
@@ -81,7 +81,8 @@ def preallocate(coll, dt, measure):
         upsert=True)
 
 def record_hit(coll, dt, measure):
-    if PREALLOC and random.random() < (1.0/200.0):
+    if PREALLOC and random.random() < (1.0/150.0):
+        preallocate(coll, dt, measure)
         preallocate(coll, dt + timedelta(days=1), measure)
     sdate = dt.strftime('%Y%m%d')
     metadata = dict(
